8897843ca4745cb013cb773bbdbdd3c8d6f5c7c5
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2025-10-03 14:11:02 +0500

    Todo App Completed

--- redux toolkit/src/App.jsx
+++ redux toolkit/src/App.jsx
@@ -6,7 +6,7 @@ import AddTodo from "./components/addTodo"
 function App() {
   return (
     <>
-      <h1>Todo</h1>
+      <h1 className='text-4xl font-bold text-center mb-10 mt-5'>Todo</h1>
       <AddTodo /> 
       <Todos />
     </>
--- redux toolkit/src/components/addTodo.jsx
+++ redux toolkit/src/components/addTodo.jsx
@@ -7,7 +7,15 @@ function addTodos () {
     const [ input, setInput ] = useState("")
     const dispatch = useDispatch()
 
+   
+
     const addTodoHandler = (e) => {
+
+        if(input.trim() === ""){
+            alert("Please enter a task")
+            return  
+        }
+
         e.preventDefault()
         dispatch(addTodo(input))
         setInput('')
@@ -18,7 +26,7 @@ function addTodos () {
             <form onSubmit={addTodoHandler} className="flex items-center gap-2">
                 <input type="text" placeholder="Add new task" onChange={(e) => setInput(e.target.value)} value={input} className="border-b w-109 p-1" />
                 <button type="submit" className="bg-black text-white rounded-sm px-3 py-1 flex items-center justify-center text-[18px] font-bold "><span className="relative bottom-[1px]">+</span></button>
-            </form>
+            </form>     
         </>
     )
 }
